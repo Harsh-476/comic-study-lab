49166a9307f931b902289d8fbb0857f79fe1854d
unknown <aabhak626@gmail.com>
2026-04-15 22:47:53 +0530

    Fixed page10

--- src/page10.jsx
+++ src/page10.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
 import "./page10.css";
 
@@ -18,41 +17,30 @@ const Projects = () => {
 
   return (
     <div className="projects-container">
-      <div className="title">Creatives: Projects</div>
-      <button className="logout" onClick={handleLogout}>LOG OUT</button>
-=======
-import "./CreativesProjects.css";
-
-const CreativesProjects = () => {
-  return (
-    <div className="container">
       {/* Title */}
       <div className="title">Creatives: Projects</div>
 
       {/* Logout */}
-      <div className="logout">LOG OUT</div>
->>>>>>> 8ecd37a (Updated font)
+      <button className="logout" onClick={handleLogout}>
+        LOG OUT
+      </button>
 
       {/* DOODLES */}
-      <img src="/images/doodle.png" alt="doodle" className="img curl-top-left" />
-      <img src="/images/doodle.png" alt="doodle" className="img curl-right" />
+      <img src={doodle} alt="doodle" className="img curl-top-left" />
+      <img src={doodle} alt="doodle" className="img curl-right" />
 
       {/* LEAVES */}
-      <img src="/images/leaf.png" alt="leaf" className="img leaf-left" />
-      <img src="/images/leaf.png" alt="leaf" className="img leaf-right-top" />
-      <img src="/images/leaf.png" alt="leaf" className="img leaf-right-mid" />
+      <img src={leaf} alt="leaf" className="img leaf-left" />
+      <img src={leaf} alt="leaf" className="img leaf-right-top" />
+      <img src={leaf} alt="leaf" className="img leaf-right-mid" />
 
       {/* STARS */}
-      <img src="/images/star.png" alt="star" className="img star-left" />
-      <img src="/images/star.png" alt="star" className="img star-bottom" />
-      <img src="/images/star.png" alt="star" className="img star-top1" />
-      <img src="/images/star.png" alt="star" className="img star-top2" />
+      <img src={star} alt="star" className="img star-left" />
+      <img src={star} alt="star" className="img star-bottom" />
+      <img src={star} alt="star" className="img star-top1" />
+      <img src={star} alt="star" className="img star-top2" />
     </div>
   );
 };
 
-<<<<<<< HEAD
 export default Projects;
-=======
-export default CreativesProjects;
->>>>>>> 8ecd37a (Updated font)
